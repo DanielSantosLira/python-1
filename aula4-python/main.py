fdfea22253d7b47469e333d8c24da7b479126835
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-09-14 16:45:11 -0300

    salvar

--- aula4-python/main.py
+++ aula4-python/main.py
@@ -2,21 +2,20 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send 
 
 app = Flask(__name__)
-#app.config["SECRET"] = "ajuianfa78fh9f78shfs768fgs7f6"
+app.config["SECRET"] = "ajuianfa78fh9f78shfs768fgs7f6"
 app.config["DEBUG"] = True
-socketio = SocketIO(app, cors_aLLowed_origins="*")
+socketio = SocketIO(app, cors_aLLowed_origins="")
 
 # Funcionalidade de enviar mensangem
 @socketio.on("message")
+
 def gerenciar_mensagem(mensagem):
     print(f"Mensagem: {mensagem}")
-    send(mensagem, broadcast=True)
+    send(mensagem, send=True)
 # criar a nossa 1ª página = 1ª rota
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-
+    return  render_template("index.html")
 # roda o nosso
 if __name__ == "__main__":  
     socketio.run(app, host="localhost")
